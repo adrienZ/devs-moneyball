c6334a5fbe6b3ffb94a0d45c888c098a404e08f2
Adrien Zaganelli <adrienzaganelli@gmail.com>
2025-08-05 17:24:15 +0200

    feat: show popular GitHub repos

--- app/pages/github.vue
+++ app/pages/github.vue
@@ -2,11 +2,32 @@
 import { gql } from '@apollo/client/core';
 import { useQuery } from '@vue/apollo-composable';
 
-const { result, loading, error } = useQuery(gql`
+interface Repo {
+  name: string;
+  stargazerCount: number;
+}
+
+interface ViewerData {
+  viewer: {
+    login: string;
+    name: string;
+    repositories: {
+      nodes: Repo[];
+    };
+  };
+}
+
+const { result, loading, error } = useQuery<ViewerData>(gql`
   query Viewer {
     viewer {
       login
       name
+      repositories(orderBy: { field: STARGAZERS, direction: DESC }, first: 5) {
+        nodes {
+          name
+          stargazerCount
+        }
+      }
     }
   }
 `);
@@ -16,6 +37,14 @@ const { result, loading, error } = useQuery(gql`
   <div>
     <p v-if="loading">Loading...</p>
     <p v-else-if="error">Error: {{ error.message }}</p>
-    <p v-else>Logged in as {{ result?.viewer.login }} ({{ result?.viewer.name }})</p>
+    <template v-else>
+      <p>Logged in as {{ result?.viewer.login }} ({{ result?.viewer.name }})</p>
+      <h2>Top Repositories</h2>
+      <ul>
+        <li v-for="repo in result?.viewer.repositories.nodes" :key="repo.name">
+          {{ repo.name }} (★ {{ repo.stargazerCount }})
+        </li>
+      </ul>
+    </template>
   </div>
 </template>
