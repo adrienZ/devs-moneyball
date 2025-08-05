dff25ab3b9c1d93ff4a3dfe294c8e5a1c372b039
Adrien Zaganelli <adrienzaganelli@gmail.com>
2025-08-05 21:22:29 +0200

    fix prod

--- server/api/github/popular-users.ts
+++ server/api/github/popular-users.ts
@@ -1,6 +1,6 @@
-import { defineEventHandler } from 'h3';
+// import { defineEventHandler } from 'h3';
+import { defineCachedEventHandler } from 'nitropack/runtime';
 import { useRuntimeConfig } from '#imports';
-import { useStorage } from "nitropack/runtime"
 // import { readFileSync } from 'node:fs';
 // import { join } from 'node:path';
 
@@ -30,37 +30,25 @@ interface PopularUsersQuery {
   search: { nodes: (User | null)[] };
 }
 
-const storageKey = 'data:github:popular';
-const maxAge = 60 * 60 * 1000; // 1 hour
-const cacheVersion = '0.1'; // Version of the cache structure
 
-export default defineEventHandler(async () => {
-  const storage = useStorage(storageKey);
-  const cached = await storage.getItem<{ data: PopularUsersQuery; cachedAt: number }>(cacheVersion);
-  
-  if (!cached || Date.now() - cached.cachedAt > maxAge) {
-      console.log(`FETCH`);
+export default defineCachedEventHandler(async () => {
 
-    const config = useRuntimeConfig();
-    const response = await fetch('https://api.github.com/graphql', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${config.public.githubToken}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ query: popularUsersQuery }),
-    });
+  const config = useRuntimeConfig();
+  const response = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${config.public.githubToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query: popularUsersQuery }),
+  });
 
-    if (!response.ok) {
-      throw new Error(`GitHub API error: ${response.status}`);
-    }
-
-    const { data } = (await response.json()) as { data: PopularUsersQuery };
-    await storage.setItem(cacheVersion, { data, cachedAt: Date.now() });
-    return data;
+  if (!response.ok) {
+    throw new Error(`GitHub API error: ${response.status}`);
   }
 
-  
-
-  return cached.data;
-});
+  const { data } = (await response.json()) as { data: PopularUsersQuery };
+  return data;
+}, {
+  maxAge: 60 * 60, // Cache for 1 hour
+})
