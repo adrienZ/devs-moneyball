64f9c8de7bda53c168ef1293cb997c84612decb6
Adrien Zaganelli <adrienzaganelli@gmail.com>
2025-10-03 11:57:08 +0200

    fix debug

--- server/plugins/auto-migrate.ts
+++ server/plugins/auto-migrate.ts
@@ -1,19 +1,18 @@
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { resolve } from "path";
-import { fileURLToPath } from "url";
+// import { migrate } from "drizzle-orm/postgres-js/migrator";
+// import { resolve } from "path";
+// import { fileURLToPath } from "url";
 import { defineNitroPlugin } from "nitropack/runtime";
-import { useDrizzle } from "../../database/client";
-import { PostgresJsDatabase } from "drizzle-orm/postgres-js/driver";
+// import { useDrizzle } from "../../database/client";
 
 export default defineNitroPlugin(async () => {
-  const db = useDrizzle();
-  if (import.meta.dev && !(db instanceof PostgresJsDatabase)) {
-    await migrate(db, {
-      migrationsFolder: resolve(
-        fileURLToPath(import.meta.url),
-        "../../../", // don't forget .nuxt folder is ancestor
-        "./database/migrations",
-      ),
-    });
-  }
+  // const db = useDrizzle();
+  // if (import.meta.dev) {
+  //   await migrate(db, {
+  //     migrationsFolder: resolve(
+  //       fileURLToPath(import.meta.url),
+  //       "../../../", // don't forget .nuxt folder is ancestor
+  //       "./database/migrations",
+  //     ),
+  //   });
+  // }
 });
